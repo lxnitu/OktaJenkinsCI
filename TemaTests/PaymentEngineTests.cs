e9334429c5653df133cc4e84aa2e9dcd45ed1a38
Nitu Alexandru-Ionut <lxnitu@hotmail.com>
2020-01-19 19:01:13 +0200

    1

--- TemaTests/PaymentEngineTests.cs
+++ TemaTests/PaymentEngineTests.cs
@@ -28,47 +28,5 @@ namespace Tema.Tests
                 account.Withdraw(50);
             });
         }
-
-        [TestMethod()]
-        public void TransferFromTest()
-        {
-            Account accountFrom = new Account();
-            Account accountTo = new Account();
-
-            accountFrom.LoadSold(100);
-
-            accountFrom.Transfer(30, accountTo);
-
-            Assert.AreEqual(accountFrom.Sold, 70);
-            Assert.AreEqual(accountTo.Sold, 30);
-        }
-
-        [TestMethod()]
-        public void TransferFromTest_ThrowsExcedes()
-        {
-            Assert.ThrowsException<System.ArgumentException>(() =>
-            {
-                Account accountFrom = new Account();
-                Account accountTo = new Account();
-
-                accountFrom.LoadSold(100);
-
-                accountFrom.Transfer(110, accountTo);
-            });
-        }
-
-        [TestMethod()]
-        public void TransferFromTest_ThrowsNullDestination()
-        {
-            Assert.ThrowsException<System.ArgumentNullException>(() =>
-            {
-                Account accountFrom = new Account();
-                Account accountTo = null;
-
-                accountFrom.LoadSold(100);
-
-                accountFrom.Transfer(10, accountTo);
-            });
-        }
     }
 }
---
+++ TemaTests/TransferTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tema.Tests
+{
+    [TestClass()]
+    public class TransferTests
+    {
+
+        [TestMethod()]
+        public void TransferFromTest()
+        {
+            Account accountFrom = new Account();
+            Account accountTo = new Account();
+
+            accountFrom.LoadSold(100);
+
+            accountFrom.Transfer(30, accountTo);
+
+            Assert.AreEqual(accountFrom.Sold, 70);
+            Assert.AreEqual(accountTo.Sold, 30);
+        }
+
+        [TestMethod()]
+        public void TransferFromTest_ThrowsExcedes()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() =>
+            {
+                Account accountFrom = new Account();
+                Account accountTo = new Account();
+
+                accountFrom.LoadSold(100);
+
+                accountFrom.Transfer(110, accountTo);
+            });
+        }
+
+        [TestMethod()]
+        public void TransferFromTest_ThrowsNullDestination()
+        {
+            Assert.ThrowsException<System.ArgumentNullException>(() =>
+            {
+                Account accountFrom = new Account();
+                Account accountTo = null;
+
+                accountFrom.LoadSold(100);
+
+                accountFrom.Transfer(10, accountTo);
+            });
+        }
+    }
+}
